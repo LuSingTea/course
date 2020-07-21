8625a072893164fc0f7762edbda97205a1270160
cunkoulaocai <1184016190@qq.com>
2020-07-21 09:38:28 +0800

    1. 更新mysql驱动版本 2. 分离server模块

--- system/src/main/java/com/course/system/SystemApplication.java
+++ system/src/main/java/com/course/system/SystemApplication.java
@@ -12,7 +12,7 @@ import org.springframework.core.env.ConfigurableEnvironment;
 @SpringBootApplication
 @EnableEurekaClient
 @ComponentScan("com.course")
-@MapperScan(basePackages = {"com.course.system.mapper"})
+@MapperScan(basePackages = {"com.course.server.mapper"})
 public class SystemApplication {
 
     private static final Logger LOG = LoggerFactory.getLogger(SystemApplication.class);
--- system/src/main/java/com/course/system/controller/TestController.java
+++ system/src/main/java/com/course/system/controller/TestController.java
@@ -1,13 +1,13 @@
 package com.course.system.controller;
 
-import com.course.system.domain.Test;
-import com.course.system.service.TestService;
-import com.netflix.discovery.converters.Auto;
+import com.course.server.domain.Test;
+import com.course.server.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+
 
 @RestController
 public class TestController {
