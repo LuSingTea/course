84e4ad4da51cedde2e8d007f5eac8ceeaf7f46e2
cunkoulaocai <1184016190@qq.com>
2020-07-28 23:31:13 +0800

    1.  完成欢迎页面开发

--- admin/src/router.js
+++ admin/src/router.js
@@ -3,7 +3,10 @@ import Router from 'vue-router'
 import Login from './views/login'
 import Admin from './views/admin'
 import Welcome from "./views/admin/welcome"
+import Chapter from "./views/admin/chapter"
+import axios from "axios"
 Vue.use(Router);
+Vue.prototype.$ajax = axios;
 
 export default new Router({
   mode: 'history',
@@ -19,12 +22,19 @@ export default new Router({
     component: Login
   }, {
     path: "/admin",
+    name: "admin",
     component: Admin,
     children: [
       {
         // 子路由不需要斜杠
         path: "welcome",
+        name: "welcome",
         component: Welcome,
+      },
+      {
+        path: "business/chapter",
+        name: "business/chapter",
+        component: Chapter
       }
     ]
   }]
