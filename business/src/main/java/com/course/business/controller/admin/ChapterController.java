758d06d1c675c3586687ff719fc4078e04409a7d
cunkoulaocai <1184016190@qq.com>
2020-07-30 17:12:09 +0800

    1. 增加后端校验 2. 添加全局异常处理

--- business/src/main/java/com/course/business/controller/admin/ChapterController.java
+++ business/src/main/java/com/course/business/controller/admin/ChapterController.java
@@ -1,11 +1,15 @@
 package com.course.business.controller.admin;
 
+import com.course.business.controller.ControllerExceptionHandler;
 import com.course.server.dto.ChapterDto;
 import com.course.server.dto.PageDto;
 import com.course.server.dto.ResponseDto;
 import com.course.server.service.ChapterService;
 import com.course.server.service.TestService;
+import com.course.server.util.ValidatorUtil;
 import com.github.pagehelper.Page;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,6 +19,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/admin/chapter")
 public class ChapterController {
+    private static final Logger LOG = LoggerFactory.getLogger(ChapterController.class);
 
     @Autowired
     private ChapterService chapterService;
@@ -29,6 +34,11 @@ public class ChapterController {
 
     @RequestMapping("/save")
     public ResponseDto save(@RequestBody ChapterDto chapterDto) {
+        // 保存校验
+        ValidatorUtil.require(chapterDto.getName(), "名称");
+        ValidatorUtil.require(chapterDto.getCourseId(), "课程ID");
+        ValidatorUtil.length(chapterDto.getCourseId(), "课程ID", 1, 8);
+
         chapterService.save(chapterDto);
         ResponseDto responseDto = new ResponseDto();
         responseDto.setContent(chapterDto);
