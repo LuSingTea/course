26ca4eef3bae248a584b7176d3db5180c484a775
cunkoulaocai <1184016190@qq.com>
2020-07-30 20:03:33 +0800

    1. 添加generator模块 2. 设计section表结构 3. 添加section生成代码

--- admin/src/main.js
+++ admin/src/main.js
@@ -5,6 +5,20 @@ import router from "./router";
 
 Vue.config.productionTip = false
 
+/**
+ * axios拦截器
+ * console.log 可以去掉了
+ */
+axios.interceptors.request.use(function (config) {
+  console.log("请求：", config);
+  return config;
+}, error => {});
+
+axios.interceptors.response.use(function (response) {
+  console.log("返回结果：", response);
+  return response;
+}, error => {});
+
 new Vue({
   router,
   render: h => h(App),
